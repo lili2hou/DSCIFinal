c2df1d4e25dc67068cfd4262dc056b7647936fa5
Navier Stokes <navierlemon@protonmail.com>
2021-12-13 17:11:12 -0300

    Changed histogram color to red.

--- random_normal.py
+++ random_normal.py
@@ -6,7 +6,7 @@ rng = np.random.default_rng()
 vals = rng.standard_normal(1000)
 
 #And make a histogram
-plt.hist(vals)
+plt.hist(vals, color='red')
 plt.ylabel('counts')
 plt.title('1000 random numbers generated from a standard normal distribution')
 plt.savefig('standard normal historgram.png')
